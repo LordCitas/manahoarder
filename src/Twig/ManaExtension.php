9235f4e8b129053c631d27aba61d1cae7c501484
LordCitas <dantre340@gmail.com>
2026-05-24 17:52:53 +0200

    símbolos de crolores en el filtro

--- src/Twig/ManaExtension.php
+++ src/Twig/ManaExtension.php
@@ -4,14 +4,23 @@ namespace App\Twig;
 
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
+use Twig\TwigFunction;
 
 class ManaExtension extends AbstractExtension
 {
     public function getFilters(): array
     {
         return [
-            // Registramos un filtro llamado 'mana_symbols'
-            new TwigFilter('mana_symbols', [$this, 'replaceManaSymbols']),
+            // Registramos un filtro llamado 'mana_symbols' marcándolo como HTML seguro
+            new TwigFilter('mana_symbols', [$this, 'replaceManaSymbols'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            // Función para renderizar un símbolo de maná individual
+            new TwigFunction('mana_symbol', [$this, 'getManaSymbol'], ['is_safe' => ['html']]),
         ];
     }
 
@@ -19,8 +28,8 @@ class ManaExtension extends AbstractExtension
     {
         // Esta expresión regular busca cualquier texto encerrado entre llaves, ej: {T}, {U}, {10}, {B/G}
         return preg_replace_callback('/\{([^}]+)\}/', function($matches) {
-            // Scryfall almacena los símbolos partidos limpios sin barras oblicuas (ej: {B/G} pasa a BG.svg)
-            $symbolName = str_replace('/', '', $matches[1]);
+            // Scryfall almacena los símbolos partidos limpios sin barras oblicuas (ej: {B/G} pasa a bg.svg)
+            $symbolName = strtolower(str_replace('/', '', $matches[1]));
             
             // Retornamos el HTML apuntando directamente al CDN de SVGs de Scryfall
             return sprintf(
@@ -30,5 +39,20 @@ class ManaExtension extends AbstractExtension
                 $matches[0]
             );
         }, $text);
+    }
+
+    public function getManaSymbol(string $symbol, string $alt = ''): string
+    {
+        $symbolName = strtolower($symbol);
+        if (empty($alt)) {
+            $alt = $symbol;
+        }
+
+        return sprintf(
+            '<img src="https://svgs.scryfall.io/card-symbols/%s.svg" class="mana-symbol" alt="%s" title="%s" style="width:20px; height:20px; vertical-align:middle;">',
+            $symbolName,
+            $alt,
+            $alt
+        );
     }
 }
